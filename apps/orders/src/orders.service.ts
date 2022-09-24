e8b3ac3d42de163fca0116c80afdca468319a986
Younes Lamkhanter <younes_lamkhanter@um5.ac.ma>
2022-09-24 19:01:23 +0100

    Add Order microservice & docker config

--- apps/orders/src/orders.service.ts
+++ apps/orders/src/orders.service.ts
@@ -1,8 +1,24 @@
 import { Injectable } from '@nestjs/common';
+import { CreateOrderRequest } from './dto/create-order.request';
+import { OrdersRepository } from './orders.repository';
 
 @Injectable()
 export class OrdersService {
-  getHello(): string {
-    return 'Hello World!';
+  constructor(private readonly ordersRepository: OrdersRepository) {}
+
+  async createOrder(request: CreateOrderRequest) {
+    const session = await this.ordersRepository.startTransaction();
+    try {
+      const order = await this.ordersRepository.create(request, { session });
+      await session.commitTransaction();
+      return order;
+    } catch (err) {
+      await session.abortTransaction();
+      throw err;
+    }
+  }
+
+  async getOrders() {
+    return this.ordersRepository.find({});
   }
 }
