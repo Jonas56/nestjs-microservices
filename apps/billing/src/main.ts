43c94df28fc3191225197947770b703be5ea16c4
Younes Lamkhanter <younes_lamkhanter@um5.ac.ma>
2022-09-26 23:04:14 +0100

    Connect apps via broker

--- apps/billing/src/main.ts
+++ apps/billing/src/main.ts
@@ -1,8 +1,11 @@
 import { NestFactory } from '@nestjs/core';
+import { RmqService } from '@app/common';
 import { BillingModule } from './billing.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(BillingModule);
-  await app.listen(3000);
+  const rmqService = app.get<RmqService>(RmqService);
+  app.connectMicroservice(rmqService.getOptions('BILLING'));
+  await app.startAllMicroservices();
 }
 bootstrap();
--- apps/orders/src/orders.controller.ts
+++ apps/orders/src/orders.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Post, Req, UseGuards } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { CreateOrderRequest } from './dto/create-order.request';
 import { OrdersService } from './orders.service';
 
@@ -7,7 +7,7 @@ export class OrdersController {
   constructor(private readonly ordersService: OrdersService) {}
 
   @Post()
-  async createOrder(@Body() request: CreateOrderRequest, @Req() req: any) {
+  async createOrder(@Body() request: CreateOrderRequest) {
     return this.ordersService.createOrder(request);
   }
 
